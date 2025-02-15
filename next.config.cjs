85604f1296160769c0a6cd4fdc217f507a346117
apurvkolte10 <apurvkolte10@gmail.com>
2025-02-15 15:21:02 +0530

    dd

--- next.config.cjs
+++ next.config.cjs
@@ -3,12 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx', 'ts'],
-  postcss: {
-    plugins: [
-      require("tailwindcss"),
-      require("autoprefixer"),
-    ],
-  },
 }
 
 module.exports = nextConfig
